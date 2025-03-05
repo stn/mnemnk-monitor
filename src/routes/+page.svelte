9352c04ca41eabe52be97077bcc60ff79a0d1f48
Akira Ishino <akira@lumilab.jp>
2025-03-05 21:47:12 +0900

    feat: 🎸 display events

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,9 +13,14 @@
     let events = $state<PublishEvent[]>([]);
     let height = $state(0);
 
+    const MAX_EVENTS = 100;
+
     $effect(() => {
       listen<PublishEvent>("mnemnk-publish", (event) => {
         events.push(event.payload);
+        if (events.length > MAX_EVENTS) {
+          events = events.slice(events.length - MAX_EVENTS);
+        }
       }).then((unlistenFn) => {
         unlisten = unlistenFn;
       });
@@ -27,15 +32,27 @@
     $effect(() => {
       window.scrollTo(0, height);
     })
+
+    function formatTime(timestamp: number): string {
+      const date = new Date(timestamp);
+      return date.toLocaleTimeString('en-US', { hour12: false });
+    }
 </script>
 
-<main class="container" bind:clientHeight={height}>
-  {#each events as event}
-    <Card>
-      <h2>{event.agent}</h2>
-      <p>{event.channel}</p>
-      <p>{JSON.stringify(event.value, null, 2)}</p>
-      <p>{new Date(event.time)}</p>
-    </Card>
-  {/each}
+<main class="container m-4" bind:clientHeight={height}>
+  <div class="grid grid-column grid-cols-12 gap-4">
+    {#each events as event}
+      <div class=""></div>
+      <div class="col-span-10 border border-gray-200 p-4 rounded-lg drop-shadow-sm">
+        <div class="drop-shadow-none">
+          <h3>
+            <span class="mr-4 mb-2">{formatTime(event.time)}</span>
+            <span class="font-bold">{event.channel} ({event.agent})</span>
+          </h3>
+        </div>
+        <p>{JSON.stringify(event.value, null, 2)}</p>
+      </div>
+      <div class=""></div>
+    {/each}
+  </div>
 </main>
